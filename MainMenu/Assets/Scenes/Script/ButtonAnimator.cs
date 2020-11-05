4735eecbc7afe541529487fe0fbc61ce7e226f55
Maryia Ondra <maryiaondra@gmail.com>
2020-11-05 23:50:28 +0300

    finished task

--- MainMenu/Assets/Scenes/Script/ButtonAnimator.cs
+++ MainMenu/Assets/Scenes/Script/ButtonAnimator.cs
@@ -1,23 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using DG.Tweening;
+using UnityEngine.UI;
 
-public class ButtonAnimator : MonoBehaviour
+public class ButtonAnimator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private float _speed;
 
     private Vector3 _buttonScale = new Vector3(0.97f, 0.94f);
     private Tween _playAnimation;
+    private Image _image;
 
     void Start()
     {
-       _playAnimation = transform.DOScale(_buttonScale, _speed).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+        _image = GetComponent<Image>();
+        AnimateButton();
     }
 
-    public void StopAnimation() 
+    private void AnimateButton() 
     {
-        if (_playAnimation.active)
-        {
-            transform.DOKill();
-        } 
+        _playAnimation = transform.DOScale(_buttonScale, _speed).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _playAnimation.Pause();
+        _image.DOColor(Color.yellow, 0.3f);        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _playAnimation.Play();
+        _image.DOColor(Color.white, 0.3f);
     }
 }
---
+++ MainMenu/Assets/Scenes/Script/ClickButton.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private UnityEngine.UI.Image _image;
+    private Vector3 _buttonScale = new Vector3(0.97f, 0.94f);
+    private Vector3 _defoultScale = new Vector3(1f, 1f);
+
+    private void Start()
+    {
+        _image = GetComponent<UnityEngine.UI.Image>();        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(_buttonScale, 0.3f);
+        _image.DOColor(Color.yellow, 0.3f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(_defoultScale, 0.3f);
+        _image.DOColor(Color.white, 0.3f);       
+    }
+}
