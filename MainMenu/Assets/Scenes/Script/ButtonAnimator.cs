63aaabbe5e523fd14d2800f71a13ddaf019d185e
Maryia Ondra <maryiaondra@gmail.com>
2020-11-05 22:32:52 +0300

    changed UI Menu

---
+++ MainMenu/Assets/Scenes/Script/ButtonAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class ButtonAnimator : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Vector3 _buttonScale = new Vector3(0.97f, 0.94f);
+    private Tween _playAnimation;
+
+    void Start()
+    {
+       _playAnimation = transform.DOScale(_buttonScale, _speed).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public void StopAnimation() 
+    {
+        if (_playAnimation.active)
+        {
+            transform.DOKill();
+        } 
+    }
+}
