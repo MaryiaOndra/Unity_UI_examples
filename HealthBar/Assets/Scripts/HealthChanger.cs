e65987b4aa315308d4a991866778b259385abbbf
Maryia Ondra <maryiaondra@gmail.com>
2020-11-06 13:56:14 +0300

    finished task

---
+++ HealthBar/Assets/Scripts/HealthChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+
+public class HealthChanger : MonoBehaviour
+{
+    private Slider _slider;
+
+    private void Start()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    public void AddHealth() 
+    {
+        _slider.value += 10;
+    }
+
+    public void TakeHealth() 
+    {
+        _slider.value -= 10;    
+    }
+}
