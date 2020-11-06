19d83ee25de2995b9a8970d2cacb0f7a6d462303
Maryia Ondra <maryiaondra@gmail.com>
2020-11-06 21:29:05 +0300

    -

--- HealthBar/Assets/Scripts/HealthChanger.cs
+++ HealthBar/Assets/Scripts/HealthChanger.cs
@@ -6,15 +6,22 @@ using UnityEngine.UI;
 public class HealthChanger : MonoBehaviour
 {
     private Slider _slider;
+    private float _currentValue;
+    private float _previousValue;
 
     private void Start()
     {
         _slider = GetComponent<Slider>();
+        _currentValue = _slider.value;
     }
 
-    public void AddHealth() 
+    public void AddHealth(float value) 
     {
-        _slider.value += 10;
+        _previousValue = _currentValue;
+        Debug.Log(_previousValue);
+        _currentValue = _currentValue + 10.0f;
+        Debug.Log(_currentValue);
+        _slider.value = Mathf.Lerp(_previousValue, _currentValue, value);        
     }
 
     public void TakeHealth() 
