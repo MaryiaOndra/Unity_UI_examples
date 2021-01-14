48780ad2c925fc892f3d566e5cadbb7af87d272c
Maryia Ondra <ya.ondra@yandex.by>
2021-01-14 15:25:46 +0300

    added toggle group

---
+++ UnityUI/Assets/RandomColor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RandomColor : MonoBehaviour
+{
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        button.onClick.AddListener(ChangeButtonColor); 
+    }
+
+    private void OnDisable()
+    {
+        button.onClick.RemoveListener(ChangeButtonColor);
+    }
+
+    private void ChangeButtonColor() 
+    {
+        GetComponent<Image>().color = RandomizeColor();
+    }
+
+    private Color RandomizeColor() 
+    {
+        return new Color
+            (
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f)
+            );
+    }
+}
